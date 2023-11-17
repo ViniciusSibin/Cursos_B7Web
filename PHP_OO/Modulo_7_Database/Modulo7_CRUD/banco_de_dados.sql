9ac196dfdaa11677b051ed03c3b183d3ac39d8f4
Vinicius Sibin <vinisibim@gmail.com>
2023-11-17 13:38:36 -0300

    Criado arquivo de banco de dados e configurado arquivo de conexão

---
+++ PHP_OO/Modulo_7_Database/Modulo7_CRUD/banco_de_dados.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE `B7web_PHP-OO`;
+
+CREATE TABLE usuarios (
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NOME VARCHAR(50) NOT NULL,
+    EMAIL VARCHAR(50) NOT NULL UNIQUE, -- Adicionando a restrição UNIQUE
+    DATA_CRIACAO DATETIME -- Corrigindo o nome da coluna
+);
+
+COMMIT;

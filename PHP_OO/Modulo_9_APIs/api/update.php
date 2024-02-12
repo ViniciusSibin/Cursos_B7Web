13ea5d9a7fa3bb318c4e676d95d5b54ecfc2a9fa
Vinicius Sibin <vinisibim@gmail.com>
2024-02-12 15:47:39 -0300

    Finalizado endpoint update

--- PHP_OO/Modulo_9_APIs/api/update.php
+++ PHP_OO/Modulo_9_APIs/api/update.php
@@ -1,7 +1,35 @@
 <?php
 require('../config.php');
 
-$array = [];
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+
+if($method === 'put'){
+    parse_str(file_get_contents('php://input'), $input);
+
+    $id = filter_var($input['id'] ?? null);
+    $title = filter_var($input['title'] ?? null);
+    $bory = filter_var($input['bory'] ?? null);
+
+    if($id && $title && $bory){
+        $sql = $pdo->prepare("UPDATE notes SET title=:title, bory=:bory WHERE id=:id");
+        $sql->bindValue(':title', $title);
+        $sql->bindValue(':bory', $bory);
+        $sql->bindValue(':id', $id);
+        $sql->execute();
+
+        $array['result'] = [
+            'msg' => 'Atualizado nota com sucesso',
+            'id' => $id,
+            'title' => $title,
+            'bory' => $bory
+        ];
+
+    } else {
+        $array['error'] = 'O title, bory ou o ID nao foram enviados.';
+    }
+} else {
+    $array['error'] = 'Metodo nao reconhecido (Correto PUT)';
+}
 
 
 require('../return.php');

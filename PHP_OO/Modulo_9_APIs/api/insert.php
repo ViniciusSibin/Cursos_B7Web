2e35a8282036e1d441f3d82f39b1fc103fbf4850
Vinicius Sibin <vinisibim@gmail.com>
2024-02-12 15:20:32 -0300

    Finalizado endpoint insert

--- PHP_OO/Modulo_9_APIs/api/insert.php
+++ PHP_OO/Modulo_9_APIs/api/insert.php
@@ -1,7 +1,26 @@
 <?php
 require('../config.php');
 
-$array = [];
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+
+if($method === 'post'){
+    $title = filter_input(INPUT_POST, 'title');
+    $bory = filter_input(INPUT_POST, 'bory');
+
+    if($title && $bory){
+        $sql = $pdo->prepare("INSERT INTO notes (title, bory) VALUES (:title, :bory)");
+        $sql->bindValue(':title', $title);
+        $sql->bindValue(':bory', $bory);
+        $sql->execute();
+
+        $array['result'] = 'Inserido nota com sucesso.';
+
+    } else {
+        $array['error'] = 'O title ou o bory nao foram enviados.';
+    }
+} else {
+    $array['error'] = 'Metodo nao reconhecido (Correto POST)';
+}
 
 
 require('../return.php');

72fba5c2a129ee8317c5e2e65d643928c40b424c
Vinicius Sibin <vinisibim@gmail.com>
2024-04-11 10:31:02 -0300

    Feito módulo de CRUD com larável

--- Laravel/Hello-world-laravel/routes/web.php
+++ Laravel/Hello-world-laravel/routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [SiteController::class, 'index']);
 Route::get('/sair', [SiteController::class, 'exit']);
 Route::get('/exercicio1', [SiteController::class, 'exercise1']);
